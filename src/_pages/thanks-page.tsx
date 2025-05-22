9b8afce2ce76ed7b89a75cda3fed1dfc8d40ad7a
vsegda_vinovat <riabko97@gmail.com>
2025-05-23 01:37:04 +0300

    new

--- src/_pages/thanks-page.tsx
+++ src/_pages/thanks-page.tsx
@@ -12,8 +12,8 @@ export const ThanksPage = () => {
 
   return (
     <div className="flex items-center justify-center w-full h-screen   ">
-      <div className="flex flex-col items-center">
-        <Title className="mb-4">
+      <div className="flex flex-col items-center max-w-[400px]">
+        <Title className="mb-4 text-center">
           Thank you for submitting your assignment!
         </Title>
 
@@ -21,8 +21,10 @@ export const ThanksPage = () => {
           <>
             <p className="text-start mb-2">Your data:</p>
             <pre className="mt-2 rounded-md bg-slate-100 p-4 mb-4">
-              <code className="text-slate-500">
-                {JSON.stringify(submittedData, null, 1)}
+              <code className="text-slate-500 text-wrap max-w-[400px]">
+                {Object.values(submittedData).map((item) => (
+                  <p className="mb-2" key={item}>{item}</p>
+                ))}
               </code>
             </pre>
           </>
